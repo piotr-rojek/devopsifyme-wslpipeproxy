c99b7068189e6eed1d50ad7943018c19bbdb1f8d
Piotr Rojek <dra9ula@gmail.com>
2023-01-17 12:13:31 +0100

    create src subfolder

--- publish.ps1
+++ publish.ps1
@@ -2,5 +2,5 @@ dotnet publish -c Release -p:PublishSingleFile=true --self-contained false
 
 New-Item -Type Directory output -Force
 Compress-Archive `
-    -Path DevOpsifyMe.WslPipeProxy\bin\Release\net7.0\win-x64\publish\* `
+    -Path src\DevOpsifyMe.WslPipeProxy\bin\Release\net7.0\win-x64\publish\* `
     -DestinationPath output\wslpipeproxy-winx64.zip -Force
