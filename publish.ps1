8403859182662102fa3874f9217fa8e66780662a
piotr-rojek <73109174+piotr-rojek@users.noreply.github.com>
2023-01-18 13:49:38 +0100

    Create docker-image.yml (#1)
    * Create docker-image.yml
    
    * add gitversion
    
    * get repo history
    
    * publish docker image
    
    * add version to action
    
    * push... not publish
    
    * dont push on PR, push latest tag on main
    
    * archive app
    
    * create temp container to get output
    
    * fix path
    
    * publish archive
    
    * create release
    
    * fix syntax
    
    * add id
    
    * set release as draft
    
    * add permission to write release
    
    * add artifact upload to a release
    
    * set proper tag and version
    
    * remove version not set on linux anyway...
    
    * remove version
    
    * release only from main
    
    * remove draft flag
    
    * remove test scripts
    
    Co-authored-by: Piotr Rojek <dra9ula@gmail.com>

--- publish.ps1
+++
@@ -1,6 +0,0 @@
-dotnet publish -c Release -p:PublishSingleFile=true --self-contained false
-
-New-Item -Type Directory output -Force
-Compress-Archive `
-    -Path src\DevOpsifyMe.WslPipeProxy\bin\Release\net7.0\win-x64\publish\* `
-    -DestinationPath output\wslpipeproxy-winx64.zip -Force
