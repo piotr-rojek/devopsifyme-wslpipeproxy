aaae0b748092220d6c6f88de2e195c0d327aab92
Piotr Rojek <dra9ula@gmail.com>
2023-01-18 13:46:33 +0100

    remove draft flag

--- docker.ps1
+++ docker.ps1
@@ -1,3 +1,9 @@
-docker build --tag devopsifyme/wslpipeproxy:0.2.0 .
-docker container rm test --force
-docker run --name test --privileged --pid=host -v /mnt/c/:/app -e INSTALL_DIR=wslpipeproxy3 wslpipeproxy
+docker build --tag devopsifyme/wslpipeproxy .
+docker create --name output devopsifyme/wslpipeproxy
+docker cp output:/install/ output/
+docker container rm output
+
+
+
+# docker container rm test --force
+# docker run --name test --privileged --pid=host -v /mnt/c/:/app -e INSTALL_DIR=wslpipeproxy3 wslpipeproxy
